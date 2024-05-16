3363a0d4096b215600c457835614e44ccbe71237
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-05-16 12:28:58 +0700

    Thêm OnConfiguring để set appsettings cho solution gahiphop_Repository Thêm 1 số field API

---
+++ GaHipHop_Service/Service/ContactService.cs
@@ -0,0 +1,18 @@
+﻿using GaHipHop_Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaHipHop_Service.Service
+{
+    public class ContactService : IContactService
+    {
+        private readonly IContactService _contactService;
+        public ContactService(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+    }
+}
