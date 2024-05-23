bae169680b786dbb1afa757559a21bd6be561bab
Phạm Quốc Đạt <134580007+datpq-dev@users.noreply.github.com>
2024-05-24 01:23:14 +0700

    add ProductRespone

--- GaHipHop_Model/DTO/Response/ProductResponse.cs
+++ GaHipHop_Model/DTO/Response/ProductResponse.cs
@@ -17,6 +17,7 @@ namespace GaHipHop_Model.DTO.Response
 
         public double ProductPrice { get; set; }
 
+        //public double CurrentPrice { get; set; }
         public int ProductQuantity { get; set; }
 
         public bool Status { get; set; }
